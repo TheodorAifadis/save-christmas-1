59f3199cd3299a214e435467e81a22fde8879cc4
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-11-29 20:48:16 +0100

    steg-3

--- statistics.js
+++ statistics.js
@@ -1,12 +1,33 @@
 
 function max(arr) {
-
+    let max = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i]
+        } 
+    }
+    return max
 }
 
 function min(arr) {
-
+    let min = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+    }
+    return min
 }
 
 function mean(arr) {
     // Ledtråd: Börja med att räkna ut summan av alla element
+    let result = 0
+    for (let i = 0; i < arr.length; i++) {
+        result = result + arr[i]
+    }
+    return result / arr.length
 }
+
+console.log(min([ 1, 2, 3, 4, 5, 6, 7, 8, 9 ])) 
+console.log(max([ 1, 2, 3, 4, 5, 6, 7, 8, 9])) 
+console.log(mean([ 1, 2, 3, 4, 5, 6, 7, 8, 9])) // 4.714285714285714
