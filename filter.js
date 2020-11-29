50b59bacb7dfe8cea02e2a94bcfb4700d8f4bc05
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-11-29 21:11:53 +0100

    steg-4

--- filter.js
+++ filter.js
@@ -1,6 +1,13 @@
 
 function filter(arr, func) {
-
+    let newArray = []
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i]) === true) {
+            newArray.push(arr[i])
+        }
+    } 
+    return newArray
 }
 
 // TEST 1
@@ -9,6 +16,11 @@ const filteredNumbers = filter(numbers, x => x < 10)
 console.log(filteredNumbers)
 
 // TEST 2 - skriv själv
-
+const animals = ['cow', 'cat', 'bear', 'bull', 'skunk', 'sloth']
+const filteredAnimals = filter(animals, x => x.length < 5)
+console.log(filteredAnimals)
 
 // TEST 3 - skriv själv
+const colours = ['red', 'blue', 'orange']
+const filteredColours = filter(colours, x => x.length < 5)
+console.log(filteredColours)
